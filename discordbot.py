6b2ba9acc1ebf879a5ebc58df162ebc0bdec4679
howling-inferno <66354085+howling-inferno@users.noreply.github.com>
2020-07-12 16:17:05 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,6 +16,20 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+@bot.command()
+async def add(ctx, a: int, b: int):
+    await ctx.send(a+b)
 
+@bot.command()
+async def multiply(ctx, a: int, b: int):
+    await ctx.send(a*b)
+
+@bot.command()
+async def greet(ctx):
+    await ctx.send(":smiley: :wave: Hello, there!")
+
+@bot.cmmands()
+async def cat(ctx):
+    await ctx.send("https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
 
 bot.run(token)
